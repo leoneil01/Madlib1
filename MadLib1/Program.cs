37a05d575b07044fcb2c8e640bc67387a630b9dd
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-20 12:29:12 +0800

    Added mad lib content

--- MadLib1/Program.cs
+++ MadLib1/Program.cs
@@ -11,6 +11,19 @@ namespace MadLib1
         static void Main(string[] args)
         {
             Console.WriteLine("Making Music at Home Mad Lib");
+
+            Console.WriteLine("\nBelow here, you will fill in all the blank spaces provided.\n");
+
+            Console.WriteLine("While I've stayed at home. I've practiced the _____ for ___ hours every _____.");
+            Console.WriteLine("My favorite key signature to play and practice in is _____ _____.");
+            Console.WriteLine("It has ___ flats and ___ sharps.");
+            Console.WriteLine("I like this key signature because it is _____ and _____");
+            Console.WriteLine("I've also gotten awesome at counting time signatures.");
+            Console.WriteLine("The time signature I love is ___, because it is _____ to count");
+            Console.WriteLine("One instrument I want to learn how to play while I'm at home is _____, because it is _____ and sounds like a _____.");
+            Console.WriteLine("My _____ likes it when I play _____, and always gives me a round of applause after my performances!");
+            Console.WriteLine("When I return to school, my teacher will be _____ of how great I am at playing my instrument.");
+            Console.WriteLine("My teacher might even think I sound like _____!");
         }
     }
 }
